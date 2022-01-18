b8e0d4c7ac9ed66d432be2c8072287c1f8473ee3
TermMC <termmc@gmail.com>
2022-01-18 12:41:16 +0000

    add BrowserRouter to app.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>{/* <Route path="/" element={<Homepage/>}></Route> */}</Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
